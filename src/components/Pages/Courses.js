86828267d8b4b73e8de5ad61f3fc0fc616ab74fb
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-25 03:22:34 +0600

    catagory list add

--- src/components/Pages/Courses.js
+++ src/components/Pages/Courses.js
@@ -1,10 +1,26 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import CatagoryList from './CatagoryList';
 const Courses = () => {
+  const [catagories,setCatagories]=useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:5000/catagories')
+    .then(res=>res.json())
+    .then(data=>setCatagories(data))
+  },[])
   return (
-    <div>
-      courses
-    </div>
+    <div className="flex flex-row container mx-auto  mt-3">
+    <div className='mr-8'>
+     <h1 className='mb-1'>Catagorys: {catagories.length}</h1>
+      <div>
+      {
+        catagories.map(ca=><CatagoryList key={ca.id} name={ca.name}></CatagoryList>)
+      }
+      </div>
+     
+      </div>
+    
+    <div className="border border-sky-500">09</div>
+  </div>
   );
 };
 
