399bd8cab8f2f8060b5cb29abf7f28ea2c00611d
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-24 23:02:45 +0600

    login register all input setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,28 @@ const Header = () => {
   return (
     <div className='bg-red-200'>
       <nav className="flex justify-center space-x-4">
-  <Link to="/" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Registerer</Link>
+  <Link to="/" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Tutor Expart</Link>
   <Link to="/" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Link>
- 
+  <Link to="/courses" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Courses</Link>
+  <Link to="/faq" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">FAQ</Link>
+  <Link to="/blogs" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Blogs</Link>
+  <label htmlFor="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer text-gray-100">
+	<span>Light</span>
+	<span className="relative">
+		<input id="Toggle1" type="checkbox" className="hidden peer" />
+		<div className="w-10 h-6 rounded-full shadow-inner bg-gray-400 peer-checked:bg-violet-400"></div>
+		<div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto bg-gray-800"></div>
+	</span>
+	<span>Dark</span>
+</label>
+  <Link to="/register" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Register</Link>
+  <Link to="/login" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</Link>
 
  
       <Link className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"></Link>
   <Link className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"><button>LogOut</button></Link>
    
-     <Link to="/register" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Register</Link>
-  <Link to="/login" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</Link>
+    
   
   
 </nav>
--- src/components/Pages/Register.js
+++ src/components/Pages/Register.js
@@ -7,16 +7,20 @@ const Register = () => {
 	<h1 className="text-2xl font-bold text-center">Register</h1>
 	<form noValidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
 		   <div className="space-y-1 text-sm">
-			<label htmlFor="username" className="block text-gray-400">name</label>
-			<input type="text" name="email" id="name" placeholder="name" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
+			<label htmlFor="username" className="block text-gray-400">Full name</label>
+			<input type="text" name="fullname" id="fullname" placeholder="full name" className="w-full px-4 py-3 rounded-md border-gray-700 bg-slate-100 text-gray-100 focus:border-violet-400" />
 		</div>
     <div className="space-y-1 text-sm">
-			<label htmlFor="username" className="block text-gray-400">Email</label>
-			<input type="email" name="email" id="email" placeholder="email" className="w-full px-4 py-3 rounded-md border-gray-700 bg-slate-50 text-gray-100 focus:border-violet-400" />
+			<label htmlFor="photourl" className="block text-gray-400">Photo URL</label>
+			<input type="text" name="photo" id="photo" placeholder="Photo URL" className="w-full px-4 py-3 rounded-md border-gray-700 bg-slate-100 text-gray-100 focus:border-violet-400" />
+		</div>
+    <div className="space-y-1 text-sm">
+			<label htmlFor="useremail" className="block text-gray-400">Email</label>
+			<input type="email" name="email" id="email" placeholder="enter email" className="w-full px-4 py-3 rounded-md border-gray-700 bg-slate-100 text-gray-100 focus:border-violet-400" />
 		</div>
 		<div className="space-y-1 text-sm">
 			<label htmlFor="password" className="block text-gray-400">Password</label>
-			<input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
+			<input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md border-gray-700 bg-slate-100 text-gray-100 focus:border-violet-400" />
 			
 		</div>
 		<button className="block w-full p-3 text-center rounded-sm text-gray-900 bg-violet-400">Register</button>
