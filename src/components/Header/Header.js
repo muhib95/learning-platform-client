3b853fb4233d188f7d2c3ac031186a2ec5adaa8a
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-24 22:16:56 +0600

    login register header design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='bg-red-200'>
+      <nav className="flex justify-center space-x-4">
+  <Link to="/" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Registerer</Link>
+  <Link to="/" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Link>
+ 
+
+ 
+      <Link className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"></Link>
+  <Link className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"><button>LogOut</button></Link>
+   
+     <Link to="/register" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Register</Link>
+  <Link to="/login" className="font-bo px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</Link>
+  
+  
+</nav>
+
     </div>
   );
 };
--- src/components/Pages/Register.js
+++ src/components/Pages/Register.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Register = () => {
   return (
     <div>
-      Register
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-900 text-gray-100 mx-auto mt-3">
+	<h1 className="text-2xl font-bold text-center">Register</h1>
+	<form noValidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+		   <div className="space-y-1 text-sm">
+			<label htmlFor="username" className="block text-gray-400">name</label>
+			<input type="text" name="email" id="name" placeholder="name" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
+		</div>
+    <div className="space-y-1 text-sm">
+			<label htmlFor="username" className="block text-gray-400">Email</label>
+			<input type="email" name="email" id="email" placeholder="email" className="w-full px-4 py-3 rounded-md border-gray-700 bg-slate-50 text-gray-100 focus:border-violet-400" />
+		</div>
+		<div className="space-y-1 text-sm">
+			<label htmlFor="password" className="block text-gray-400">Password</label>
+			<input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
+			
+		</div>
+		<button className="block w-full p-3 text-center rounded-sm text-gray-900 bg-violet-400">Register</button>
+	</form>
+	
+	
+	<p className="text-xs text-center sm:px-6 text-gray-400">already have an account?
+		<Link to='/login' rel="noopener noreferrer"  className="underline text-gray-100">Login</Link>
+	</p>
+</div>
     </div>
   );
 };
